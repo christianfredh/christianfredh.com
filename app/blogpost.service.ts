649fc42e01afb4875fc87251c8c36d1db10ed5c6
Christian Fredh <christian.fredh@gmail.com>
2016-06-06 12:43:16 +0200

    Some styling + edited date condition

--- app/blogpost.service.ts
+++ app/blogpost.service.ts
@@ -5,12 +5,15 @@ import { BlogPost } from './blogpost'
 export class BlogPostService {
     
     public getBlogPosts() : Promise<BlogPost[]> {
+        
+        var sameDate = new Date();
+        
         return Promise.resolve([
             { 
                 id: 'first', 
                 title: 'Latest blog post',
-                created: new Date(),
-                lastEdited: new Date(),
+                created: sameDate,
+                lastEdited: sameDate,
                 markdownContent: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
             },
             { 
@@ -24,11 +27,13 @@ export class BlogPostService {
     }
     
     public getBlogPost(id: string) : Promise<BlogPost> {
+        
+        var sameDate = new Date();
         return Promise.resolve({ 
             id: id, 
             title: 'Latest blog post ' + id,
-            created: new Date(),
-            lastEdited: new Date(),
+            created: sameDate,
+            lastEdited: sameDate,
             markdownContent: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
         });
     }
